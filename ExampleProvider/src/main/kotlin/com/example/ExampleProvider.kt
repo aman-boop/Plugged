d442b1b1329f05d502b154115a976d76574e0fa4
aman-boop <58726697+aman-boop@users.noreply.github.com>
2026-07-22 04:00:07 +0000

    fix: correct return statement in load function and ensure episodes are reversed

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -44,17 +44,18 @@ class ExampleProvider : MainAPI() { // All providers must be an instance of Main
         val posterUrl = fixUrl(document.select("img.wp-post-image").attr("src"))
         val description = document.select("div.entry-content p").text()
 
-        val episodes = document.select("ul li a").mapNotNull { element ->
+        return document.select("ul li a").mapNotNull { element ->
             val episodeTitle = element.text()
             val episodeUrl = fixUrl(element.attr("href"))
             newEpisode(episodeTitle, episodeUrl)
-        }.reverse() // Reverse the list to have the latest episode first
 
-        return newTvSeriesLoadResponse(title, url, TvType.Anime) {
-            this.posterUrl = posterUrl
-            this.description = description
-            this.episodes = episodes
+            listOf(
+                newTvSeriesLoadResponse(title, url, TvType.Anime) {
+                    this.posterUrl = posterUrl
+                    this.description = description
+                    this.episodes = episodes
+                }
+            ).reverse() // Reverse the list so that the latest episode is first
         }
     }
-
 }
